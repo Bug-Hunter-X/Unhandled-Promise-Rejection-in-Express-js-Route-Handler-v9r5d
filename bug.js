25b3d534a76142b336e60197520b0c556aaf5a09
BugHunter-X <askzenai@gmail.com>
2025-01-16 22:32:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  // Asynchronous operation that might fail
+  someAsyncOperation().then(result => {
+    res.send(result);
+  }).catch(error => {
+    // Error handling missing here! 
+  });
+});
+app.listen(3000, () => console.log('Server listening on port 3000'));
